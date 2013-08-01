88ea398bd9f77389fcf6df8141f100b5ca2569d7
Vladislav Babkin <hotab@Vladislavs-MacBook-Pro.local>
2013-08-01 15:34:55 +0300

    rewrite for cocos;

--- NSBezierPathCodeBuilder.m
+++ NSBezierPathCodeBuilder.m
@@ -13,24 +13,33 @@
 
 - (NSString *) codeForBezierPoints {
 	NSArray *points = [self effectiveBezierPoints];
+    
+    /*
+     ccBezierConfig bezier1;
+     bezier1.controlPoint_1 = ccp(50, 220);
+     bezier1.controlPoint_2 = ccp(220, 220);
+     bezier1.endPosition = ccp(220,50);
+     */
 	
 	NSMutableArray *lines = [NSMutableArray array];
 	
-	[lines addObject:@"NSBezierPath *bp = [[NSBezierPath alloc] init];"];
+	//[lines addObject:@"NSBezierPath *bp = [[NSBezierPath alloc] init];"];
 	for (NSUInteger i = 0; i < [points count]; ++i) {
 		BezierPoint *point = [points objectAtIndex:i];
-		if (i == 0) {
-			[lines addObject:[NSString stringWithFormat:@"[bp moveToPoint:NSMakePoint(%0.2f, %0.2f)]", [point mainPoint].x, [point mainPoint].y]];
+		if (i == 0){
+            [lines addObject:[NSString stringWithFormat:@"object%ld.position = ccp(%0.1f,%0.1f);",i/2+1,
+                [point mainPoint].x, [point mainPoint].y]];
 		} else {
-			[lines addObject:[NSString stringWithFormat:@"[bp curveToPoint:NSMakePoint(%0.2f, %0.2f) controlPoint1:NSMakePoint(%0.2f, %0.2f) controlPoint2:NSMakePoint(%0.2f, %0.2f)];", 
-							  [point mainPoint].x, [point mainPoint].y,
-							  [point controlPoint1].x, [point controlPoint1].y,
-							  [point controlPoint2].x, [point controlPoint2].y]];
+            [lines addObject:[NSString stringWithFormat:@"ccBezierConfig bezier%ld;",i]];
+            [lines addObject:[NSString stringWithFormat:@"bezier%ld.controlPoint_1 = ccp(%0.1f, %0.1f);",i,[point controlPoint1].x, [point controlPoint1].y]];
+            [lines addObject:[NSString stringWithFormat:@"bezier%ld.controlPoint_2 = ccp(%0.1f, %0.1f);",i,[point controlPoint2].x, [point controlPoint2].y]];
+            [lines addObject:[NSString stringWithFormat:@"bezier%ld.endPosition = ccp(%0.1f, %0.1f);",i,[point mainPoint].x, [point mainPoint].y]];
+            [lines addObject:[NSString stringWithFormat:@"object%ld.position = ccp(%0.1f,%0.1f);",i+1,
+                              [point mainPoint].x, [point mainPoint].y]];
 		}
 	}
 	
-	[lines addObject:@"[bp stroke];"];
-	[lines addObject:@"[bp release];"];
+	//[lines addObject:@"[bp release];"];
 	
 	return [lines componentsJoinedByString:@"\n"];
 }
